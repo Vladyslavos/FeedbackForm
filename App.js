85798f22aad4303207b091b205ba41d0c64f0409
IvanLun <126349571+IvanLun@users.noreply.github.com>
2023-06-13 13:11:23 +0300

    Some bugs have been fixed

--- App.js
+++ App.js
@@ -1,5 +1,3 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import FeedbackForm from "./components/FeedbackForm";
 
 export default function App() {
